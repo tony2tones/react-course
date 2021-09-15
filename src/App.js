01b58bc172ae5e1ccbd59befeda28462c861c0b7
tony2tones <anthonymarques@outlook.com>
2021-09-15 20:11:17 +0200

    learning React from scratch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import Todo from "./components/Todo";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello!
+      <h1>My Todos</h1>
+      <Todo text="Learn React" />
+      <Todo text="Master React" />
+      <Todo text="Explore the  React" />
     </div>
   );
 }
 
 export default App;
+
+// const button = document.querySelector("button");
+
+let modal;
+let backdrop;
+
+// button.addEventListener("click", showModalHandler);
+
+function showModalHandler() {
+  if (modal) {
+    return;
+  }
+
+  modal = document.createElement("div");
+  modal.className = "modal";
+
+  const modalText = document.createElement("p");
+  modalText.textContent = "Are you sure?";
+
+  const modalCancelAction = document.createElement("button");
+  modalCancelAction.textContent = "Cancel";
+  modalCancelAction.className = "btn btn--alt";
+  modalCancelAction.addEventListener("click", closeModalHandler);
+
+  const modalConfirmAction = document.createElement("button");
+  modalConfirmAction.textContent = "Confirm";
+  modalConfirmAction.className = "btn";
+  modalConfirmAction.addEventListener("click", closeModalHandler);
+
+  modal.append(modalText);
+  modal.append(modalCancelAction);
+  modal.append(modalConfirmAction);
+
+  document.body.append(modal);
+
+  backdrop = document.createElement("div");
+  backdrop.className = "backdrop";
+
+  backdrop.addEventListener("click", closeModalHandler);
+
+  document.body.append(backdrop);
+}
+
+function closeModalHandler() {
+  modal.remove();
+  modal = null;
+
+  backdrop.remove();
+  backdrop = null;
+}
