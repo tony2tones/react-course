45e586e0aa3cd6ba3f93749a548c90f7b5e6af31
tony2tones <anthonymarques@outlook.com>
2021-09-16 17:35:33 +0200

    Adding new pages and routing to project

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -10,6 +10,9 @@ function Todo(props) {
     setModalIsOpen(true);
   }
 
+function closeModalHandler() {
+  setModalIsOpen(false);
+}
   return (
     <div className="card">
       <h2>{props.text}</h2>
@@ -18,8 +21,8 @@ function Todo(props) {
           Delete
         </button>
       </div>
-      {modelIsOpen && <Modal />}
-      {modelIsOpen && <Backdrop />}
+      {modelIsOpen && <Modal onCancel={closeModalHandler} />}
+      {modelIsOpen && <Backdrop onCancel={closeModalHandler} />}
       
     </div>
   );
