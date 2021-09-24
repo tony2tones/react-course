b2ccda1711f0aeaf39e4c6c4afd9418fe65e2a7f
tony2tones <anthonymarques@outlook.com>
2021-09-24 14:32:11 +0200

    created layout component

---
+++ src/components/layout/layout.js
@@ -0,0 +1,10 @@
+import MainNavigation from './MainNavigation';
+import classes from './Layout.module.css';
+
+function Layout() {
+return <div>
+    <MainNavigation />
+</div>
+}
+
+export default Layout;
